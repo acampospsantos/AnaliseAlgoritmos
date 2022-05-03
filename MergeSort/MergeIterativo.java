7478607585c165581a45176bcc8713d5037ad840
antsu comuna <acampospsantos@hotmail.com>
2022-05-02 22:02:32 -0300

    update iterative

--- MergeSort/MergeIterativo.java
+++ MergeSort/MergeIterativo.java
@@ -22,50 +22,49 @@ public class MergeIterativo {
     }
 
     
-    //Iterative mergesort function to sort arr[0...n-1]
+    //Função MergeSort Iterativa para ordenar Array[0...n-1]
     static void mergeSort(int arr[], int n) {
 
-        // For current size of subarrays to be merged curr_size varies from 1 to n/2
+        // Para o tamanho atual dos subarrays serem mesclados, curr_size varia de 1 a n/2
         int curr_size;
 
-        // For picking starting index of left subarray to be merged
+        // Para escolher o índice inicial do subarray esquerdo a ser mesclado
         int left_start;
 
-        // Merge subarrays in bottom up manner. First merge subarrays of size 1 to create sorted subarrays of size 2, then merge
-        // subarrays of size 2 to create sorted subarrays of size 4, and so on.
+        // Mescla subarray's de maneira ascendente. Primeiro mescla os subarrays de tamanho 1 para criar subarrays ordenados de tamanho 2 e, em seguida, mescla
+        // subarrays de tamanho 2 para criar subarrays classificados de tamanho 4 e assim por diante.
         for (curr_size = 1; curr_size <= n - 1; curr_size = 2 * curr_size) {
-            // Pick starting point of different subarrays of current size
+            // Escolhe o ponto de partida de diferentes subarrays de tamanho atual
             for (left_start = 0; left_start < n - 1; left_start += 2 * curr_size) {
-                // Find ending point of left subarray. mid+1 is starting point of right
+                // Encontra o ponto final do subarranjo esquerdo. mid+1 é o ponto inicial da direita
                 int mid = Math.min(left_start + curr_size - 1, n - 1);
 
-                int right_end = Math.min(left_start
-                        + 2 * curr_size - 1, n - 1);
+                int right_end = Math.min(left_start + 2 * curr_size - 1, n - 1);
 
-                // Merge Subarrays arr[left_start...mid] & arr[mid+1...right_end]
+                // Mescla subarrays arr[left_start...mid] e arr[mid+1...right_end
                 merge(arr, left_start, mid, right_end);
             }
         }
     }
 
     
-    //Function to merge the two haves arr[l..m] and arr[m+1..r] of array arr[]    
+    //Função para mesclar os dois possui arr[l..m] e arr[m+1..r] do array arr[]  
     static void merge(int arr[], int l, int m, int r) {
         int i, j, k;
         int n1 = m - l + 1;
         int n2 = r - m;
 
-        /* create temp arrays */
+        //Cria Array Temporário
         int L[] = new int[n1];
         int R[] = new int[n2];
 
-        // Copy data to temp arrays L[]and R[]        
+        // Copia(passa) os dados para os Arrays temporários L[] e R[]        
         for (i = 0; i < n1; i++)
             L[i] = arr[l + i];
         for (j = 0; j < n2; j++)
             R[j] = arr[m + 1 + j];
 
-        // Merge the temp arrays back into arr[l..r]
+        // Mescla os arrays temporários de volta ao array[l..r]
         i = 0;
         j = 0;
         k = l;
@@ -80,14 +79,14 @@ public class MergeIterativo {
             k=k+1;
         }
 
-        //Copy the remaining elements of L[], if there are any
+        // Copia os elementos restantes de L[], se tiver algum
         while (i < n1) {
             arr[k] = L[i];
             i=i+1;
             k=k+1;
         }
 
-        // Copy the remaining elements of R[], if there are any
+        // Copia os elementos restantes de R[], se tiver algum
         while (j < n2) {
             arr[k] = R[j];
             j=j+1;
